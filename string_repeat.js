bdb7eb23aa9ca7793155c32f8e1502490587a65a
Erica Mendez <ecmendez25@gmail.com>
2018-10-08 18:03:02 -0400

    string_repeat
    The challenge was to make a function that will repeat a string (x) number of times in Javascript

---
+++ string_repeat.js
@@ -0,0 +1,10 @@
+ // JavaScript solution for "String repeat"
+
+ /*
+ The challenge was to make a function that will repeat
+ a string (x) number of times
+ */
+
+ function repeatStr (n, s) {
+  return s.repeat(n);
+}
