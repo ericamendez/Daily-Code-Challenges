ef8a04ab2eb199a9384038bee69203c4cc376cdf
Eriquette <ecmendez25@gmail.com>
2018-11-21 19:27:10 -0500

    inserting items into array and converting into string

---
+++ 6kyu/create_phone_number.js
@@ -0,0 +1,18 @@
+/*
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+*/
+
+
+function createPhoneNumber(numbers){
+  if(numbers.length === 10){
+    numbers.splice(0,0,"(")
+    numbers.splice(4,0,") ")
+    numbers.splice(8,0,"-")
+    return numbers.join('')
+  }
+}
+
+
+Test.assertEquals(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
