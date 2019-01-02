f73ebff4ae17ad9e73fc392c158c77771b1deced
Eriquette <ecmendez25@gmail.com>
2019-01-02 18:46:45 -0500

    How many animals are there

---
+++ codewars/7kyu/count_animal_string.js
@@ -0,0 +1,12 @@
+function countAnimals(sentence) {
+    let num = sentence.match(/\d/g);
+    if(num == null){
+        return 0
+    }else{
+        let numArr = num.map(item => Number(item))
+        let answer = numArr.reduce((a,b) => a+b)
+        return answer
+    }
+}
+
+console.log(countAnimals("I see 3 zebras, 5 lions and 6 giraffes."))
