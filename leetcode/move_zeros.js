b14b4b49edfa4f2052fe42530af086902d6f55d6
ericamendez <ecmendez25@gmail.com>
2020-07-25 19:17:14 -0400

    move zeros

--- leetcode/move_zeros.js
+++ leetcode/move_zeros.js
@@ -31,3 +31,38 @@ var moveZeroes = function (nums) {
 
     return nums
 };
+
+/**
+ *
+ * Things I learned from other solution when looping through an array that is being modified: 
+ * - uses while loop since the for loop will get confused if we modify the array we loop through
+ * - The condition in the while loop is that while i (counter for numbers > 0) is less than nums.length (the length of the continously modified array):
+ *      - if the number[i]  is 0, remove that number (which modifies array) and increase count(keeps track of the amount of zeros)
+ *          and do not increase the i because the element has been remove and there will be a new element in this current i
+ *      - else increase i. i will eventually be equal to nums.length as we loop through the array
+ *      - from there, push the amount of zeros ccount specifies to end of num array
+ * 
+ * (Leetcode common solution)
+ var moveZeroes = function (nums) {
+     // move o's
+     // In place
+     //
+
+     let i = 0;
+     let count = 0;
+     while (i < nums.length) {
+         if (nums[i] === 0) {
+             nums.splice(i, 1);
+             count++;
+         } else {
+             i++;
+         }
+     }
+
+     while (count > 0) {
+         nums.push(0);
+         count--;
+     }
+
+ };
+ */
