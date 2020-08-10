b0590d4883a4558e17134c6e6bae4f082f546ea5
ericamendez <ecmendez25@gmail.com>
2020-08-09 23:16:43 -0400

    unfinished

--- leetcode/rotting_oranges.js
+++ leetcode/rotting_oranges.js
@@ -28,17 +28,54 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    let count = 0
+    let copy = [...grid]
+    // console.log(copy)
+    // while there are no ones (if one is isolted, return -1)
 
+    while(grid.flat().includes(1)) {
+        for(let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                let oneCheck = []
+
+                if (grid[i][j] === 1){
+                    oneCheck.push(grid[i - 1][j])
+                    oneCheck.push(grid[i + 1][j])
+                    oneCheck.push(grid[i][j - 1])
+                    oneCheck.push(grid[i][j] + 1)
+
+                    if(oneCheck.filter(el => el !== undefined || el !== 0).length === 0) {
+                        return -1
+                    }
+                } else if (grid[i][j] === 2) {
+                    if (i != 0 && grid[i - 1][j] !== 0) {
+                        grid[i - 1][j] = 2
+                    } 
+                    if (i != grid.length - 1 && grid[i + 1][j] !== 0) {
+                        grid[i + 1][j] = 2
+                    }
+                    if (j != 0 && grid[i][j - 1][i] !== 0) {
+                        grid[i][j-1][i] = 2
+                    }
+                    if (j != grid.length && grid[i][j + 1] !== 0) {
+                        grid[i][j+1] = 2
+                    }
+                }
+            }
+        }
+        // console.log(copy)
+
+        count++
+    }
+
+    return count
 };
 
-orangesRotting([
-    [2, 1, 1],
-    [1, 1, 0],
-    [0, 1, 1]
-])
 
 let edge = [
     [2, 1, 1],
     [0, 1, 1],
     [1, 0, 1]
 ]
+
+console.log(orangesRotting(edge))
