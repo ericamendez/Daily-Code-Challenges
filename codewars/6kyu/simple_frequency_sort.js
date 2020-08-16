ab13a4e06eb77673cf1d64ec0a1abea76a766746
ericamendez <ecmendez25@gmail.com>
2020-08-16 17:08:31 -0400

    my solution for simple frequesncy sort + another solution usign short circuit evaluation

--- codewars/6kyu/simple_frequency_sort.js
+++ codewars/6kyu/simple_frequency_sort.js
@@ -8,31 +8,31 @@
  Good luck!
  */
 
-// function solve(arr) {
-//     let obj = {}
-//     let answer = []
+function solve(arr) {
+    let obj = {}
+    let answer = []
 
-//     arr.forEach(num => {
-//         obj[num] = (obj[num] + 1) || 1
-//     })
+    arr.forEach(num => {
+        obj[num] = (obj[num] + 1) || 1
+    })
 
-//     let sortedPairs = Object.entries(obj).sort((a, b) => {
-//         if (b[1] === a[1]){
-//             return a[0] - b[0]
-//         }
-//         return b[1] - a[1]
-//     })
+    let sortedPairs = Object.entries(obj).sort((a, b) => {
+        if (b[1] === a[1]){
+            return a[0] - b[0]
+        }
+        return b[1] - a[1]
+    })
     
-//     sortedPairs.forEach(pair => {
-//         for(let i = 0; i < pair[1]; i++) {
-//             answer.push(Number(pair[0]))
-//         }
-//     })
+    sortedPairs.forEach(pair => {
+        for(let i = 0; i < pair[1]; i++) {
+            answer.push(Number(pair[0]))
+        }
+    })
 
-//     return answer
-// }
+    return answer
+}
 
-// solve([2, 3, 5, 3, 7, 9, 5, 3, 7])
+solve([2, 3, 5, 3, 7, 9, 5, 3, 7])
 
 
 /**
@@ -40,20 +40,14 @@
  * 1) They made an object with key of each number in the array and value of how many times the number appears, similar to my solution.
  * 2) Instead of pushing to a new array like I did, they sorted a copy of the arr argument. 
  *      - if the frequency of the second element is smaller then the first, it will be truthy, if not compare the values 
- * 
- * function solve(arr) {
-     var r = {}
-     for (var n of arr) r[n] = r[n] + 1 || 1
-     return arr.slice().sort((a, b) => r[b] - r[a] || a - b)
- }
  */
 
- function solve(arr) {
-     var r = {}
-     for (var n of arr) r[n] = r[n] + 1 || 1
+//  function solve(arr) {
+//      var r = {}
+//      for (var n of arr) r[n] = r[n] + 1 || 1
 
-     console.log(arr.slice())
-     return arr.slice().sort((a, b) => r[b] - r[a] || a - b)
- }
+//      console.log(arr.slice())
+//      return arr.slice().sort((a, b) => r[b] - r[a] || a - b)
+//  }
 
- solve([2, 3, 5, 3, 7, 9, 5, 3, 7])
+//  solve([2, 3, 5, 3, 7, 9, 5, 3, 7])
