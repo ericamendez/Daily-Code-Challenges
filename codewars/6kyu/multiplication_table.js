1ee7afe2925df5040e9814e2be5d0a8cbe6ceb1d
ericamendez <ecmendez25@gmail.com>
2020-08-27 19:23:57 -0400

    refactor

--- codewars/6kyu/multiplication_table.js
+++ codewars/6kyu/multiplication_table.js
@@ -10,17 +10,13 @@ for example, when given size is 3:
 for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
  */
 
-function multiplicationTable(size) {
-    let answer = []
-    let firstArray = Array.from(Array(size), (_, i) => i + 1)
+function multiplicationTable(row, col) {
+    let answer = [Array.from(Array(col), (_, i) => i + 1)]
 
-    answer.push(firstArray)
-
-    for(let i = 2; i <= size; i++) {
-        answer.push(firstArray.map((el) => el * i))
+    for(let i = 2; i <= row; i++) {
+        answer.push(answer[0].map((el) => el * i))
     }
     
-    console.log(answer)
     return answer
 }
 
