aa36f00f7b709f37b522674d7a55728c5d7ac2b9
ericamendez <ecmendez25@gmail.com>
2020-08-28 21:00:11 -0400

    sort the array odd

---
+++ codewars/6kyu/sort_the_arrau.py
@@ -0,0 +1,11 @@
+'''
+You have an array of numbers.
+Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+Example
+'''
+
+def sort_array(source_array):
+    
