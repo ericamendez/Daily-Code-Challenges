21e17645ed616fb4fdba22fd24e6fe4f74c0bdd3
ericamendez <ecmendez25@gmail.com>
2020-09-01 20:17:14 -0400

    refactored count_odds

--- codewars/8kyu/count_odds.js
+++ codewars/8kyu/count_odds.js
@@ -4,16 +4,10 @@ Count Odd Numbers below n
 -Given a number n, return the number of positive odd numbers below n
 */
 
-let list = [];
-
 function oddCount(n){
-  let int = Number(n);
-  if(int > 0){
-    for(let i = 1; i <= int; i++){
-      if(i % 2 != 0){
-        list.push(i);
-      }
-    }
+  let count = 0
+  for(let i = 1; i <= Number(n); i++){
+    i % 2 != 0 ? count += 1 : null
   }
-  return list.length
+  return count
 }
