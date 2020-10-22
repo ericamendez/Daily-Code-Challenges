b4d271052a226969ddb57a60cf2ac2357fc606aa
ericamendez <ecmendez25@gmail.com>
2020-10-22 17:49:04 -0400

    divide array into chunk arrays of given size

---
+++ misc-challenges/chunk.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  let answer = []
+
+  for (let i = 0; i < array.length / size; i++) {
+    let sliceStart = i * size
+
+    answer.push([...array.slice(sliceStart, sliceStart + size)])
+  }
+
+  return answer
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
