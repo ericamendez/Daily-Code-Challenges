1429faf6959612566e5002fe818d3b7a5467020a
ericamendez <ecmendez25@gmail.com>
2020-11-09 07:24:04 -0500

    greed is good dice game

---
+++ codewars/5kyu/greed_is_good.js
@@ -0,0 +1,72 @@
+/**
+ * Greed is a dice game played with five six - sided dice.Your mission, should you choose to accept it, is to score a
+ throw according to these rules.You will always be given an array with five six - sided dice values.
+
+ Three 1 's => 1000 points
+ Three 6 's =>  600 points
+ Three 5 's =>  500 points
+ Three 4 's =>  400 points
+ Three 3 's =>  300 points
+ Three 2 's =>  200 points
+ One 1 => 100 points
+ One 5 => 50 point
+ A single die can only be counted once in each roll.For example, a given "5"
+ can only count as part of a triplet(contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+ Example scoring
+
+ Throw Score
+ -- -- -- -- - -- -- -- -- -- -- -- -- --
+ 5 1 3 4 1 250: 50(
+   for the 5) + 2 * 100(
+   for the 1 s)
+ 1 1 1 3 1 1100: 1000(
+   for three 1 s) + 100(
+   for the other 1)
+ 2 4 4 5 4 450: 400(
+   for three 4 s) + 50(
+   for the 5)
+ In some languages, it is possible to mutate the input to the
+ function.This is something that you should never do.If you mutate the input, you will not be able to pass all the tests.
+
+ https: //www.codewars.com/kata/5270d0d18625160ada0000e4/train/javascript
+ */
+
+function score(dices) {
+  let countObj = {}
+
+  dices.forEach(dice => {
+    countObj[dice] = (countObj[dice] +1) || 1
+  });
+
+  let points = 0
+
+  for (const roll in countObj) {
+    switch (roll) {
+      case '1':
+        countObj[roll] >= 3 ? points += 1000 + (countObj[roll] % 3 * 100) : points += countObj[roll] % 3 * 100;
+        break
+      case '2':
+        countObj[roll] >= 3 ? points += 200 : null;
+        break
+      case '3':
+        countObj[roll] >= 3 ? points += 300 : null;
+        break
+      case '4':
+        countObj[roll] >= 3 ? points += 400 : null;
+        break
+      case '5':
+        countObj[roll] >= 3 ? points += 500 + (countObj[roll] % 3 * 50) : points += countObj[roll] % 3 * 50
+        break
+      case '6':
+        countObj[roll] >= 3 ? points += 600 : null;
+        break
+      default:
+        break;
+    }
+  }
+
+  return points
+}
+
+console.log(score([4, 4, 4, 3, 3]))
