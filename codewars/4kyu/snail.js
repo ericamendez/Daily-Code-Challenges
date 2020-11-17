705c1df8d6b6df5698d1725eac98ae36be9b5922
ericamendez <ecmendez25@gmail.com>
2020-11-17 14:00:16 -0500

    automorphic

---
+++ codewars/4kyu/snail.js
@@ -0,0 +1,48 @@
+/**
+ * Snail Sort
+ Given an n x n array,
+ return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+ array = [
+   [1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9]
+ ]
+ snail(array)# => [1, 2, 3, 6, 9, 8, 7, 4, 5]
+ For better understanding, please follow the numbers of the next array consecutively:
+
+   array = [
+     [1, 2, 3],  [0][0], [0][1], [0][2] => [1][2], [2][2] end of right => [1][0] => [1][1] end of left
+     [8, 9, 4],
+     [7, 6, 5]
+   ]
+ snail(array)# => [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ This image will illustrate things more clearly:
+
+
+   NOTE: The idea is not sort the elements from the lowest value to the highest;
+ the idea is to traverse the 2 - d array in a clockwise snailshell pattern.
+
+ NOTE 2: The 0x0(empty matrix) is represented as en empty array inside an array[[]].
+ */
+
+function snail(array) {
+  let answer = []
+  let upCount = 0
+  let downCount = 0
+  
+  // we only have to iterate the amount of paths?
+  let paths = array[0].length * 2 - 1
+
+  for (let i = 0; i < paths; i++) {
+    console.log(paths);
+  }
+
+  return answer
+}
+
+console.log(snail([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]));
