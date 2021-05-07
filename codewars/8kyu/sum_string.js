b9bef4535f05bbe2ad589e4240b65e6e50f66774
ericamendez <ecmendez25@gmail.com>
2021-05-07 19:34:21 -0400

    sum strings

---
+++ codewars/8kyu/sum_string.js
@@ -0,0 +1,10 @@
+/**
+ * Create a
+ function that takes 2 positive integers in form of a string as an input, and outputs the sum(also as a string):
+
+   Example: (Input1, Input2-- > Output)
+ */
+
+function sumStr(a, b) {
+  return (parseInt(a) + parseInt(b)).toString()
+}
