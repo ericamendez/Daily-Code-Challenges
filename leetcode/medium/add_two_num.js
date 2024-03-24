f0db2c7a2a38b844322c8f78548e911cd8409120
ericamendez <ecmendez25@gmail.com>
2024-03-24 13:57:44 -0400

    reverse to make equal

--- leetcode/medium/add_two_num.js
+++ leetcode/medium/add_two_num.js
@@ -20,35 +20,14 @@ Explanation: 342 + 465 = 807.
  * @return {ListNode}
  */
 
-let ll1 = {
-    val: 2,
-    next: {
-        val: 4,
-        next: {
-            val: 3,
-            next: null
-        }
-    }
-}
-
-let ll2 = {
-    val: 5,
-    next: {
-        val: 6,
-        next: {
-            val: 4,
-            next: null
-        }
-    }
-}
-
+//BRUTE FORCE 👎🏽
 let arrayToLinkedList = (arr) => {
     let head = {
         val: Number(arr[0]),
         next: null
     }
 
-    for(let i = 1; i < arr.length; i++){
+    for (let i = 1; i < arr.length; i++) {
         head = {
             val: Number(arr[i]),
             next: head
@@ -62,7 +41,7 @@ let linkedListToArray = (ll) => {
     let head = ll
     let arr = []
 
-    while(head){
+    while (head) {
         arr.push(head.val)
         head = head.next
     }
@@ -70,32 +49,76 @@ let linkedListToArray = (ll) => {
     return arr
 }
 
-var addTwoNumbers = function(l1, l2) {
-    let arr1  = linkedListToArray(l1)
+let formatArrayToNumber = (arr) => {
+    console.log('format', BigInt(arr.join('')), typeof Number(arr.join('')));
+    return BigInt(arr.join(''))
+}
+
+var addTwoNumbers = function (l1, l2) {
+    let arr1 = linkedListToArray(l1)
     let arr2 = linkedListToArray(l2)
 
-    console.log(arr1.join(''))
-    const sum = Number(arr1.join('')) + Number(arr2.join(''))
-
-    //check if int is bigint
-    let matcha = arr1.join('')
-    console.log(typeof Number(matcha));
-
+    const sum = formatArrayToNumber(arr1) + formatArrayToNumber(arr2)
+    console.log('sum', sum)
 
     const sumFormattedToArray = sum.toString().split('')
-    console.log(sumFormattedToArray)
 
     return arrayToLinkedList(sumFormattedToArray)
 };
 
-//console.log(addTwoNumbers(ll1, ll2))
-
 // edge case
-let a1 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-let a2 = [5,6,4]
+let a1 = [2, 4, 3]
+let a2 = [5, 6, 4]
 
 
 const a1ToLL = arrayToLinkedList(a1)
 const a2ToLL = arrayToLinkedList(a2)
 
-console.log(addTwoNumbers(a1ToLL, a2ToLL))
+// console.log("finallll", addTwoNumbers(a1ToLL, a2ToLL))
+// console.log("finallll", linkedListToArray(addTwoNumbers(a1ToLL, a2ToLL)));
+
+
+
+//OPTIMIZED SOLUTION, no deconstructing and reconstructing linked list
+//O(n) time complexity
+
+// constructor function that starts off a linked list
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+var addTwoNumbers2 = function (l1, l2) {
+    let carry = 0;
+    let prevNode = new ListNode();
+    const headNode = prevNode;
+    while (l1 || l2 || carry) {
+        let val1 = 0;
+        let val2 = 0;
+
+        if (l1) {
+            val1 = l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+
+            val2 = l2.val;
+
+            l2 = l2.next;
+
+        }
+        const sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
+        const digit = sum % 10;
+        const currentNode = new ListNode(digit);
+        console.log('currentNode',currentNode)
+        console.log('prevNode',prevNode);
+        prevNode.next = currentNode;
+        console.log(`prevNode.next`,prevNode);
+        prevNode = currentNode;
+        console.log('headNode',headNode);
+    }
+    return headNode.next;
+}
+
+console.log(addTwoNumbers2(a1ToLL, a2ToLL));
