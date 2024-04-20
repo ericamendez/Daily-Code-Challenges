824ccbc4eb761797f2e0601cf4542aa333543382
ericamendez <ecmendez25@gmail.com>
2024-04-20 12:22:58 -0400

    binary search

--- misc-challenges/algorithms/binary_search.js
+++ misc-challenges/algorithms/binary_search.js
@@ -1,10 +1,14 @@
 export default function bs_list(haystack, needle ) {
     let lo = 0
     let hi = haystack.length
+    //do while will execute the loop at least once while regular while will check the ocndition first
     do {
+        //find the middle index
         const middle = Math.floor(lo+(hi-lo)/2)
+        //middle value
         const value = haystack[middle]
 
+        //if the middle value is equal to the
         if (value === needle){
             return true
         } else if(needle > value) {
@@ -16,3 +20,32 @@ export default function bs_list(haystack, needle ) {
 
     return false
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
