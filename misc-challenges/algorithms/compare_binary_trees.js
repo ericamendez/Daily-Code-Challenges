0949478fdf3591dfe31571a62651a3ccf8290211
ericamendez <ecmendez25@gmail.com>
2024-04-21 12:36:50 -0400

    bst

---
+++ misc-challenges/algorithms/compare_binary_trees.js
@@ -0,0 +1,19 @@
+// a, b both Binary Nodes
+function compare(a, b) {
+    //structural check
+    if (a === null && b === null) {
+        return true
+    }
+
+    //structural check
+    if (a === null || b === null) {
+        return false
+    }
+
+    //value check
+    if (a.value !== b.value) {
+        return false
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right)
+}
