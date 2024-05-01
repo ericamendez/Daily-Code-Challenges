b6baa5c34be01b48fd4e339778b3462a15c80765
ericamendez <ecmendez25@gmail.com>
2024-05-01 10:02:31 -0400

    moving zeros to end of array in python

--- codewars/5kyu/moving_zeros_to_end.py
+++ codewars/5kyu/moving_zeros_to_end.py
@@ -1,4 +1,8 @@
-# Complexity time O(n), space O(1)
+# https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
+
+# Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+# move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 
 def move_zeros(lst):
     if not lst:
@@ -16,3 +20,5 @@ def move_zeros(lst):
             i += 1
             
     return lst
+
+# Complexity time O(n), space O(1)
