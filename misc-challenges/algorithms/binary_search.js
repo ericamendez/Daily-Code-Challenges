28172bf19e86131b706ffefd41f095e810c34c27
ericamendez <ecmendez25@gmail.com>
2024-07-12 16:12:58 -0400

    binary serach

--- misc-challenges/algorithms/binary_search.js
+++ misc-challenges/algorithms/binary_search.js
@@ -1,30 +1,25 @@
-export default function bs_list(haystack, needle ) {
-    let lo = 0
-    let hi = haystack.length
-    //do while will execute the loop at least once while regular while will check the ocndition first
-    do {
-        //find the middle index
-        const middle = Math.floor(lo+(hi-lo)/2)
-        //middle value
-        const value = haystack[middle]
+function search(nums, target){
+    let [lo, hi] = [0, nums.length - 1]
+    
+    while(lo <= hi){
+        const mid = (hi + lo) >> 1; //equivalent to Math.floor((hi - lo)/2)
+        const value = nums[mid]
 
-        //if the middle value is equal to the
-        if (value === needle){
-            return true
-        } else if(needle > value) {
-            lo = middle + 1
-        } else {
-            hi = middle
-        }
-    } while(lo < hi)
+        const isTarget = value === target
+        if (isTarget) return mid
 
-    return false
+        const isTargetBigger = value < target
+        if (isTargetBigger) lo = mid + 1
+
+        const istargetLess = value > target
+        if (istargetLess) hi = mid - 1
+    }
+
+    return -1
 }
 
 
-
-
-
+console.log(search([-1,0,2,4,6,8], 4));
 
 
 
